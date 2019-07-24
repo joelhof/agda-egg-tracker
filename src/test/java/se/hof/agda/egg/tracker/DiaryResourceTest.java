cd6deb5d8a0baf2e1ac3a4251a2a26483fe1bb6c
Joel Hofgren <hofgren.joel@gmail.com>
2019-07-24 23:21:48 +0200

    Add test for POST diary entry. Note that Maven sets current working directory to project root

--- src/test/java/se/hof/agda/egg/tracker/DiaryResourceTest.java
+++ src/test/java/se/hof/agda/egg/tracker/DiaryResourceTest.java
@@ -1,7 +1,12 @@
 package se.hof.agda.egg.tracker;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -16,6 +21,15 @@ public class DiaryResourceTest {
           .then()
              .statusCode(200)
              .body(is("hello"));
+    }
+
+    @Test
+    public void testPOSTDiaryEntry() throws IOException {
+        String validDiaryEntry = Files.readString(Paths.get("src/test/resources/POST-valid-diary.json"));
+        given().body(validDiaryEntry)
+               .contentType(ContentType.JSON)
+        .when().post("diary/entry")
+        .then().assertThat().statusCode(200);
     }
 
 }
