6d06cf4c5add3c03eaeceafd694c322ec2170ce5
Joel Hofgren <hofgren.joel@gmail.com>
2019-07-24 21:59:27 +0200

    Initial commit of bootstraped quarkus app

---
+++ src/test/java/se/hof/agda/egg/tracker/NativeDiaryResourceIT.java
@@ -0,0 +1,9 @@
+package se.hof.agda.egg.tracker;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeDiaryResourceIT extends DiaryResourceTest {
+
+    // Execute the same tests but in native mode.
+}
