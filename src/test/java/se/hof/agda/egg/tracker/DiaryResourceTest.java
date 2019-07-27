a8c394e871e014336f272765936c6a61d68be08c
Joel Hofgren <hofgren.joel@gmail.com>
2019-07-27 12:28:29 +0200

    Add test for json based POST-endpoint.

--- src/test/java/se/hof/agda/egg/tracker/DiaryResourceTest.java
+++ src/test/java/se/hof/agda/egg/tracker/DiaryResourceTest.java
@@ -9,19 +9,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class DiaryResourceTest {
-
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
-    }
 
     @Test
     public void testPOSTDiaryEntry() throws IOException {
