4f06a73bac0a855b5a006cfe7eae175101e0f80e
Joel Hofgren <hofgren.joel@gmail.com>
2019-07-28 23:53:10 +0200

    Store diary entries in database.

--- src/main/java/se/hof/agda/egg/tracker/DiaryResource.java
+++ src/main/java/se/hof/agda/egg/tracker/DiaryResource.java
@@ -1,19 +1,40 @@
 package se.hof.agda.egg.tracker;
 
+import io.agroal.api.AgroalDataSource;
 import se.hof.agda.egg.tracker.dto.DiaryEntryDTO;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Path("diary")
 public class DiaryResource {
+
+    @Inject
+    AgroalDataSource eggDataSource;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/entry")
     public String createDiaryEntry(DiaryEntryDTO body) {
-        return "nr of eggs posted " + body.getEggs();
+        String sql = "INSERT INTO diary.entries (eggs, timestamp)" +
+                "values (?,?)";
+        try (
+                Connection dbConnection = eggDataSource.getConnection();
+                PreparedStatement ps = dbConnection.prepareStatement(sql)) {
+            ps.setInt(1, body.getEggs());
+            ps.setLong(2,body.getTimestamp());
+            int res = ps.executeUpdate();
+            ps.close();
+            return "nr of eggs posted " + body.getEggs();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Egg diary was not updated. ", e);
+        }
     }
 
 }
