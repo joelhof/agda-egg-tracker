33bffc4c7c88505ff9ee495aeec66a1cc75facf7
Joel Hofgren <hofgren.joel@gmail.com>
2019-08-26 11:16:48 +0200

    Configure webpack so that 'npm run dev' works to launch webapp.

--- src/main/resources/META-INF/resources/index.js
+++ src/main/resources/META-INF/resources/index.js
@@ -1,2 +1,16 @@
 import '@vaadin/vaadin-text-field';
 import '@vaadin/vaadin-button';
+
+window.addEventListener('load', () => {
+     initUI();
+});
+
+function initUI() {
+  const eggCount = document.querySelector('#eggs');
+  const addButton = document.querySelector('#post');
+
+  addButton.addEventListener('click', e => {
+    console.log('You have reported ' + eggCount.value + ' eggs!');
+    eggCount.value = '';
+  });
+}
--- src/main/resources/META-INF/resources/webpack.config.js
+++ src/main/resources/META-INF/resources/webpack.config.js
@@ -4,15 +4,20 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = ({ mode }) => {
     return {
         mode,
+        entry: {
+                app: ['./index.js']
+        },
         plugins: [
             new HtmlWebpackPlugin({
                 template: './post-current-egg-count.html'
             }),
-            new CopyWebpackPlugin({
+            new CopyWebpackPlugin([
+            {
                 context: 'node_modules/@webcomponents/webcomponentsjs',
                 from: '**/*.js',
                 to: 'webcomponents'
-            })
+            }
+            ])
          ],
          devtool: mode === 'development' ? 'source-map' : 'none'
     };
