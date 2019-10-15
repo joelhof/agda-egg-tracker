0e627be16dea4379fec7c61528f97c618a1ba418
Joel Hofgren <hofgren.joel@gmail.com>
2019-10-15 23:29:44 +0200

    Add MicroProfile ConfigSource to parse DATABASE_URL.

---
+++ src/main/java/se/hof/agda/egg/tracker/DatasourceConfig.java
@@ -0,0 +1,53 @@
+package se.hof.agda.egg.tracker;
+
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+
+public class DatasourceConfig implements ConfigSource {
+
+    Map<String, String> properties = new HashMap<>();
+
+    public DatasourceConfig() {
+        URI dbUri = null;
+        try {
+            dbUri = new URI(System.getenv("DATABASE_URL"));
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+            properties.put("quarkus.datasource.url", dbUrl);
+            properties.put("quarkus.datasource.username", username);
+            properties.put("quarkus.datasource.password", password);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+    @Override
+    public String getValue(String s) {
+        return properties.get(s);
+    }
+
+    @Override
+    public String getName() {
+        return DatasourceConfig.class.getName();
+    }
+
+    @Override
+    public Set<String> getPropertyNames() {
+        return Set.of("quarkus.datasource.url",
+                      "quarkus.datasource.username",
+                      "quarkus.datasource.password");
+    }
+}
