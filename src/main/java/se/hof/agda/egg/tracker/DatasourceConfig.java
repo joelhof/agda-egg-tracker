5332b590a1b651b30971c72e359d579dc18668ff
Joel Hofgren <hofgren.joel@gmail.com>
2019-10-16 23:49:23 +0200

    Don't fail if database URL is not set.

--- src/main/java/se/hof/agda/egg/tracker/DatasourceConfig.java
+++ src/main/java/se/hof/agda/egg/tracker/DatasourceConfig.java
@@ -5,19 +5,35 @@ import org.eclipse.microprofile.config.spi.ConfigSource;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.*;
-import java.util.logging.Logger;
 
 public class DatasourceConfig implements ConfigSource {
 
     Map<String, String> properties = new HashMap<>();
 
     public DatasourceConfig() {
-        URI dbUri = null;
+        String databaseUrl = System.getenv("DATABASE_URL");
+        if (databaseUrl != null) {
+            parseDatabaseUrl(databaseUrl);
+        } else
+            setDefaultProperties();
+    }
+
+    private void setDefaultProperties() {
+        properties.put("quarkus.datasource.url", "");
+        properties.put("quarkus.datasource.username", "");
+        properties.put("quarkus.datasource.password", "");
+    }
+
+    private void parseDatabaseUrl(String databaseUrl) {
         try {
-            dbUri = new URI(System.getenv("DATABASE_URL"));
+            URI dbUri = new URI(databaseUrl);
             String username = dbUri.getUserInfo().split(":")[0];
             String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+            String dbUrl = "jdbc:postgresql://"
+                    + dbUri.getHost() + ':'
+                    + dbUri.getPort()
+                    + dbUri.getPath()
+                    + "?sslmode=require";
 
             properties.put("quarkus.datasource.url", dbUrl);
             properties.put("quarkus.datasource.username", username);
@@ -48,5 +64,10 @@ public class DatasourceConfig implements ConfigSource {
                 Arrays.asList("quarkus.datasource.url",
                               "quarkus.datasource.username",
                               "quarkus.datasource.password"));
+    }
+
+    @Override
+    public int getOrdinal() {
+        return 101;
     }
 }
