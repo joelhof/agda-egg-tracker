ad32c8e7449596a294e0a6f4c9c6062bebe30fad
Joel Hofgren <hofgren.joel@gmail.com>
2019-10-20 20:12:57 +0200

    Import clean up

--- src/main/java/se/hof/agda/egg/tracker/DiaryResource.java
+++ src/main/java/se/hof/agda/egg/tracker/DiaryResource.java
@@ -1,12 +1,13 @@
 package se.hof.agda.egg.tracker;
 
 import io.agroal.api.AgroalDataSource;
-import org.flywaydb.core.Flyway;
 import se.hof.agda.egg.tracker.dto.DiaryEntryDTO;
 
-import javax.enterprise.inject.Default;
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
