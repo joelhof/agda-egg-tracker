f6595be56d1c057b3796c307aacdcd52977ba4e7
Joel Hofgren <hofgren.joel@gmail.com>
2019-11-03 21:36:49 +0100

    Show a simple message in case save fails.
    Note method to provide dynamic text to notification.

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,40 +1,68 @@
 import '@vaadin/vaadin-text-field';
 import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-notification';
 
 window.addEventListener('load', () => {
      initUI();
 });
 
 function initUI() {
-  const eggCount = document.querySelector('#eggs');
-  const addButton = document.querySelector('#post');
+  const notification = document.querySelector('vaadin-notification');
 
-  eggCount.addEventListener('change', e => {
-       const current = eggCount.value;
-       addButton.disabled = !eggCount.checkValidity();
-  });
-  eggCount.addEventListener('input', e => {
-        const current = eggCount.value;
-        addButton.disabled = !eggCount.checkValidity();
-  });
-
-  addButton.addEventListener('click', e => {
-
-    const data = {
-        'eggs': eggCount.value,
-        'timestamp': Date.now()
-    };
-    console.log(data);
-    try {
-        const response = postEggs(data);
-        response.then()
-        .catch()
-        console.log(response.status);
-        eggCount.value = '';
-    } catch (error) {
-        console.log('Error, egg count not posted', error);
+  notification.renderer = function(root, n) {
+      console.log('render notification', n);
+    // Check if there is a content generated with the previous renderer call not to recreate it.
+    if (root.firstElementChild) {
+      return;
     }
-  });
+    const container = window.document.createElement('div');
+    const notificationMessage = window.document.createElement('b');
+    notificationMessage.textContent = n.error;
+    container.appendChild(notificationMessage);
+    root.appendChild(container);
+  }
+
+  const eggCount = initEggCountField();
+  const addButton = initSaveButton();
+
+    function initSaveButton() {
+        const addButton = document.querySelector('#post');
+        addButton.addEventListener('click', e => {
+            const data = {
+                'eggs': eggCount.value,
+                'timestamp': Date.now()
+            };
+            console.log(data);
+            try {
+                const response = postEggs(data);
+                response.then()
+                    .catch(error => {
+                        notification.error = error;
+                        notification.open();
+                        console.log("show notification", error, " notification: ", notification);
+                    } );
+                console.log(response.status);
+                eggCount.value = '';
+            }
+            catch (error) {
+                console.log('Error, egg count not posted', error);
+            }
+        });
+        return addButton;
+    }
+
+    function initEggCountField() {
+        const eggCount = document.querySelector('#eggs');
+        eggCount.addEventListener('change', e => {
+            const current = eggCount.value;
+            addButton.disabled = !eggCount.checkValidity();
+        });
+        eggCount.addEventListener('input', e => {
+            const current = eggCount.value;
+            addButton.disabled = !eggCount.checkValidity();
+        });
+        return eggCount;
+    }
 }
 
 async function postEggs(data) {
