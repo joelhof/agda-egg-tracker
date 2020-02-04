14ea4a998e58eda05c6cf39c9c9fa8684c89b2d5
Joel Hofgren <hofgren.joel@gmail.com>
2020-02-04 21:18:41 +0100

    Destroy chart before loading with data.
    Fixes bug where multiple charts would amass for each fetch call resulting in poor UX.

--- src/main/webapp/statistics.js
+++ src/main/webapp/statistics.js
@@ -2,32 +2,31 @@ import Chart from 'chart.js';
 import '@vaadin/vaadin-text-field';
 
 let currentPeriod;
+let chart;
 
 window.addEventListener('load', () => {
      initControls();
-     fetchDiaryEntries();
+     initChart();
+     fetchDiaryEntries(currentPeriod);
 });
 
-
 function displayChart(entries) {
-
+    chart.destroy();
     const averager = (accumulator, currentValue) => {
-        accumulator.sum   = accumulator.sum + currentValue;
+        accumulator.sum = accumulator.sum + currentValue;
         accumulator.count = ++accumulator.count;
         return accumulator;
     };
     var averageAcc = entries.map(e => e.eggs)
         .reduce(averager, {sum: 0.0, count: 0});
-    var average = averageAcc.sum / averageAcc.count;
     console.log('init statistics ui');
     updateMean(averageAcc);
     var ctx = document.getElementById("egg-chart");
-    var chart = new Chart(ctx, {
+    chart = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: entries.map(e => new Date(e.timestamp))
                            .map(d => toISODate(d)),
-            
             datasets: [
                 {   
                     backgroundColor: '#adffe5',
@@ -46,32 +45,60 @@ function displayChart(entries) {
     });
 }
 
+function initChart() {
+    var ctx = document.getElementById("egg-chart");
+    chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [],
+            datasets: [
+                {   
+                    backgroundColor: '#adffe5',
+                    fill: false,
+                    label: 'Dagiliga ägg',
+                    data: []
+                },
+                {
+                    backgroundColor: '',
+                    label: 'Rullande 7-dagars medelvärde',
+                    type: 'line',
+                    data: []
+                }
+            ]
+        }
+    });
+}
+
 function initControls() {
     const today = new Date();
     const oneYearAgo = new Date();
     oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
     currentPeriod = new Object();
     currentPeriod['startDate'] = toISODate(oneYearAgo);
-    currentPeriod['endDate'] = toISODate(today)
+    currentPeriod['endDate'] = toISODate(today);8
     const startDateField = document.querySelector('#startdate');
     startDateField.value = currentPeriod.startDate;
     startDateField.addEventListener('change', e => {
         const startDate = startDateField.value;
+        currentPeriod.startDate = startDate;
+        fetchDiaryEntries(currentPeriod);
         console.log('start date changed', startDate);
     });
     const endDateField = document.querySelector('#enddate');
     endDateField.value = currentPeriod.endDate;
     endDateField.addEventListener('change', e => {
         const endDate = endDateField.value;
+        currentPeriod.endDate = endDate;
+        fetchDiaryEntries(currentPeriod);
         console.log('end date changed', endDate);
     });
 }
 
-function fetchDiaryEntries() {
+function fetchDiaryEntries(period) {
 
     const response = fetchEntries(
-        currentPeriod.startDate,
-        currentPeriod.endDate
+        period.startDate,
+        period.endDate
     );
     response.then(r => r.json())
             .then(entries => {
@@ -89,9 +116,9 @@ async function fetchEntries(start, end) {
         headers: { 'Accept': 'application/json' }
      });
 
-    //const data = [{"eggs":6,"timestamp":1574977754934},{"eggs":7,"timestamp":1574893748527},{"eggs":5,"timestamp":1574632510021},{"eggs":2,"timestamp":1573593347407},{"eggs":6,"timestamp":1572818173386},{"eggs":5,"timestamp":1572468610972}]
-    //const response = {};
-    //response.json = () => data;
+    // const data = [{"eggs":6,"timestamp":1574977754934},{"eggs":7,"timestamp":1574893748527},{"eggs":5,"timestamp":1574632510021},{"eggs":2,"timestamp":1573593347407},{"eggs":6,"timestamp":1572818173386},{"eggs":5,"timestamp":1572468610972}]
+    // const response = {};
+    // response.json = () => data;
     return response;
 }
 
